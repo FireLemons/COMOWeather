9214487060d4ac3d7f11dc44bcc8c3476a9a6cad
Shen Yang <shenyang64@gmail.com>
2019-11-14 21:35:43 -0600

    refactored nav out of index, build script generates html files

--- build.js
+++ build.js
@@ -9,11 +9,26 @@ let aboutTemplate,
     navHeaderPartial
 
 function onConfigMakerFilesLoaded(){
-
   if(configMakerTemplate && navPartial){
-    console.log(Mustache.render(configMakerTemplate, {}, {
+    fs.writeFile('./configMaker.html', Mustache.render(configMakerTemplate, {}, {
       nav: navPartial
-    }))
+    })).then(() => {
+      console.log('generated configMaker.html')
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+}
+
+function onIndexFilesLoaded(){
+  if(indexTemplate && navPartial){
+    fs.writeFile('./index.html', Mustache.render(indexTemplate, {}, {
+      nav: navPartial
+    })).then(() => {
+      console.log('generated index.html')
+    }).catch((err) => {
+      console.error(err)
+    })
   }
 }
 
@@ -22,14 +37,23 @@ fs.readFile('./templates/configMaker.mustache', 'utf8')
     configMakerTemplate = template
     onConfigMakerFilesLoaded()
   }).catch((err) => {
-    console.log(err)
+    console.error(err)
   })
 
+
+fs.readFile('./templates/index.mustache', 'utf8')
+  .then((template) => {
+    indexTemplate = template
+    onIndexFilesLoaded()
+  }).catch((err) => {
+    console.error(err)
+  })
 
 fs.readFile('./templates/nav.mustache', 'utf8')
   .then((html) => {
     navPartial = html
     onConfigMakerFilesLoaded()
+    onIndexFilesLoaded()
   }).catch((err) => {
-    console.log(err)
+    console.error(err)
   })

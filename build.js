4f60f7a08e0f5684aa3d278ba9bfb381653b2f30
Shen Yang <shenyang64@gmail.com>
2019-11-15 17:42:04 -0600

    refactored nav out of about

--- build.js
+++ build.js
@@ -8,9 +8,25 @@ let aboutTemplate,
     navPartial,
     navHeaderPartial
 
+function onAboutFilesLoaded(){
+  if(aboutTemplate && navPartial){
+    fs.writeFile('./about.html', Mustache.render(aboutTemplate, {
+      'js-possible': false
+    }, {
+      nav: navPartial
+    })).then(() => {
+      console.log('generated about.html')
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+}
+
 function onConfigMakerFilesLoaded(){
   if(configMakerTemplate && navPartial){
-    fs.writeFile('./configMaker.html', Mustache.render(configMakerTemplate, {}, {
+    fs.writeFile('./configMaker.html', Mustache.render(configMakerTemplate, {
+      'js-possible': true
+    }, {
       nav: navPartial
     })).then(() => {
       console.log('generated configMaker.html')
@@ -22,7 +38,9 @@ function onConfigMakerFilesLoaded(){
 
 function onIndexFilesLoaded(){
   if(indexTemplate && navPartial){
-    fs.writeFile('./index.html', Mustache.render(indexTemplate, {}, {
+    fs.writeFile('./index.html', Mustache.render(indexTemplate, {
+      'js-possible': true
+    }, {
       nav: navPartial
     })).then(() => {
       console.log('generated index.html')
@@ -31,6 +49,14 @@ function onIndexFilesLoaded(){
     })
   }
 }
+
+fs.readFile('./templates/about.mustache', 'utf8')
+  .then((template) => {
+    aboutTemplate = template
+    onAboutFilesLoaded()
+  }).catch((err) => {
+    console.error(err)
+  })
 
 fs.readFile('./templates/configMaker.mustache', 'utf8')
   .then((template) => {
@@ -52,6 +78,7 @@ fs.readFile('./templates/index.mustache', 'utf8')
 fs.readFile('./templates/nav.mustache', 'utf8')
   .then((html) => {
     navPartial = html
+    onAboutFilesLoaded()
     onConfigMakerFilesLoaded()
     onIndexFilesLoaded()
   }).catch((err) => {

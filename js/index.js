7e0d944ecb667cb7da408a95eb2bad0fd8bcc2bd
Shen Yang <shenyang64@gmail.com>
2019-11-22 19:55:57 -0600

    created load listner for twitter frame

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let maxWaitIntervals = 16
+
+let twitterTimelineLoadListener = setInterval(() => {
+  if(!maxWaitIntervals){
+    console.error('Waiting for twitter timeline timed out.')
+  }
+
+  let timelineFrame = document.getElementById('twitter-widget-0')
+
+  if(timelineFrame || !maxWaitIntervals){
+    clearInterval(twitterTimelineLoadListener)
+
+    console.log(timelineFrame)
+  }
+
+  maxWaitIntervals--
+}, 200)

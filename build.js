118fc864c869c20c116956bf16cac4639a89c781
firelemons <shenyang64@gmail.com>
2020-06-30 19:35:28 -0500

    reduced global data

--- build.js
+++ build.js
@@ -212,29 +212,19 @@ function buildCSS (generatedFilePath, primarySource, secondarySources) {
   })
 }
 
-const trackedFiles = {
-  sources: {
-    'about.mustache':         new SourceFile('./templates/about.mustache'),
-    'configMaker.mustache':   new SourceFile('./templates/configMaker.mustache'),
-    'configMaker.scss':       new SourceFile('./css/sass/configMaker.scss'),
-    'index.mustache':         new SourceFile('./templates/index.mustache'),
-    'aboutModal.mustache':    new SourceFile('./templates/aboutModal.mustache'),
-    'nav.mustache':           new SourceFile('./templates/nav.mustache'),
-    'nav.scss':               new SourceFile('./css/sass/_nav.scss'),
-    'sharedStyles.mustache':  new SourceFile('./templates/sharedStyles.mustache'),
-    'sharedScripts.mustache': new SourceFile('./templates/sharedScripts.mustache'),
-    'theme.scss':             new SourceFile('./css/sass/_theme.scss'),
-    'themeForms.scss':        new SourceFile('./css/sass/_themeForms.scss')
-  },
-  generatedFiles: [
-    './about.html',
-    './configMaker/index.html',
-    './index.html',
-    './css/configMaker.css'
-  ]
+const sources = {
+  'about.mustache':         new SourceFile('./templates/about.mustache'),
+  'configMaker.mustache':   new SourceFile('./templates/configMaker.mustache'),
+  'configMaker.scss':       new SourceFile('./css/sass/configMaker.scss'),
+  'index.mustache':         new SourceFile('./templates/index.mustache'),
+  'aboutModal.mustache':    new SourceFile('./templates/aboutModal.mustache'),
+  'nav.mustache':           new SourceFile('./templates/nav.mustache'),
+  'nav.scss':               new SourceFile('./css/sass/_nav.scss'),
+  'sharedStyles.mustache':  new SourceFile('./templates/sharedStyles.mustache'),
+  'sharedScripts.mustache': new SourceFile('./templates/sharedScripts.mustache'),
+  'theme.scss':             new SourceFile('./css/sass/_theme.scss'),
+  'themeForms.scss':        new SourceFile('./css/sass/_themeForms.scss')
 }
-
-const sources = trackedFiles.sources
 
 const buildTrees = [
   new DependencyTree(
